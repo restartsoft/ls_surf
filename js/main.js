4aca11311901bbe65001ed85e5eadbade75b81d2
Сергей <restartsoft@live.ru>
2024-01-22 22:01:32 +0300

    add jquery slider scroll

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const fullscreenMenuClose = document.querySelector(".fullscreen-menu__close");
 
 hamburgerBtn.addEventListener("click", e => {
     e.preventDefault();
-    
+
     fullscreenMenu.classList.add("fullscreen-menu-active");
     body.classList.add("body-noscroll");
 });
@@ -18,26 +18,51 @@ fullscreenMenuClose.addEventListener("click", e => {
 });
 
 //slider scroll
-
 $(document).ready(() => {
-    let sliderBtn = $(".slider__link");
 
-    sliderBtn.on("click", function(e)
-    {
-        let slider = $(".slider__list");
-        console.log(slider.width());
+    const sliderBtn = $(".slider__link");
+    const slider = $(".slider__list");
+    const sliderItems = $(".slider__item");
+    const maxIndex = sliderItems.length - 1;
+
+    sliderBtn.on("click", function (e) {
+
+        let sliderWidth = slider.width();
+        let curSlide = sliderItems.filter("#currentSlide");
+        let curIndex = sliderItems.index(curSlide);
 
         e.preventDefault();
+        curSlide.removeAttr("id");
 
-        if ($(this).is("#slider-leftBtn"))
-        {
-            slider.css("right", "0px");
-            console.log("left");
+        if ($(this).is("#slider-leftBtn")) {
+            if (curIndex > 0) {
+                slider.css("right", `${sliderWidth * (curIndex - 1)}px`);
+                $(sliderItems[curIndex - 1]).attr("id", "currentSlide");
+            }
+            else {
+                slider.css("right", `${sliderWidth * (maxIndex)}px`);
+                $(sliderItems[maxIndex]).attr("id", "currentSlide");
+            }
         }
-        else
-        {
-            console.log("right");
-            slider.css("right", `${slider.width()}px`);
+        else {
+            if (curIndex < maxIndex) {
+                slider.css("right", `${sliderWidth * (curIndex + 1)}px`);
+                $(sliderItems[curIndex + 1]).attr("id", "currentSlide");
+            }
+            else {
+                slider.css("right", "0px");
+                $(sliderItems[0]).attr("id", "currentSlide");
+            }
         }
     });
+
+    $(window).on("resize", function () {
+        
+        let sliderWidth = slider.width();
+        let curSlide = sliderItems.filter("#currentSlide");
+        let curIndex = sliderItems.index(curSlide);
+
+        slider.css("right", `${sliderWidth * curIndex}px`);
+    });
+
 });
